ffa887b81a5c04f832a8cfebbc93540325bf6c52
thuhaphamdlb <thuhaphamdlb@gmail.com>
2020-01-16 11:06:45 +0700

    Third muoi sau thang mot lan 2

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -15,7 +15,7 @@ import cucumber.api.testng.CucumberFeatureWrapper;
 import cucumber.api.testng.TestNGCucumberRunner;
 
 @CucumberOptions(
-        features = "src/test/java/features/resetTransactions.feature",
+        features = "src/test/java/features",
         glue = {"tests"},
         tags = {"~@Ignore"},
         format = {
